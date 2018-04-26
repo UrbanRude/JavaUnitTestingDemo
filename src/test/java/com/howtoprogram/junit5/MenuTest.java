6357ef523e8e77dcfe15ce296dba00f6de3b647f
UrbanRude <urban.rude@gmail.com>
2018-04-26 18:32:01 -0500

    Test mockeado

--- src/test/java/com/howtoprogram/junit5/MenuTest.java
+++ src/test/java/com/howtoprogram/junit5/MenuTest.java
@@ -3,11 +3,24 @@ package com.howtoprogram.junit5;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.howtoprogram.junit5.services.Election;
+import com.howtoprogram.junit5.services.IElection;
 
 
 class MenuTest {
 	@Test
 	void test() {
-		//IElection election = new Election();
+		IElection election = new Election();
+		assertEquals(100,election.getNumberOfVotesByParty("Axity"));
+	}
+	
+	@Test
+	void testMockito() {
+		Election election = mock(Election.class);
+		when(election.getCandidateNameByParty("prueba")).thenReturn("El Homero");
+		assertEquals("El Homero",election.getCandidateNameByParty("prueba"));
 	}
 }
